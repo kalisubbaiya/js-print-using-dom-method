9f3f2a533008e25ffa6f084670b17d3d0bdef199
kaliraj <kalisdvk96@gmail.com>
2022-12-28 23:07:53 +0530

    add print method

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+let elements = document.getElementsByName("myName");
+console.log(elements[0].tagName);
+
+let  head3= document.getElementsByClassName("hello");
+console.log(head3[0].innerText);
+
+let  head4 = document.getElementsByTagName("h1");
+console.log(head4[0].innerText);
