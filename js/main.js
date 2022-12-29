26eb77be83ddecf65db2dadc13206780ec06d93e
kaliraj <kalisdvk96@gmail.com>
2022-12-29 09:21:08 +0530

    print dom mehtod

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 let elements = document.getElementsByName("myName");
 console.log(elements[0].tagName);
 
-let  head3= document.getElementsByClassName("hello");
-console.log(head3[0].innerText);
+let  head1= document.getElementsByClassName("hello");
+console.log(head1[0].innerText);
 
-let  head4 = document.getElementsByTagName("h1");
-console.log(head4[0].innerText);
+let  head2 = document.getElementsByTagName("h1");
+console.log(head2[0].innerText);
